0c51d32bacc688f16d8a677938f3d293ab6dca87
Donatien Vachette <donatien.vachette@hotmail.com>
2025-05-21 16:22:17 +0200

    [AJOUT] Création de la structure des packages

--- src/main/java/iut/g3s2e2/Main.java
+++
@@ -1,7 +0,0 @@
-package iut.g3s2e2;
-import iut.g3s2e2.demo.DemoSwing;
-public class Main {
-    public static void main(String[] args) {
-        DemoSwing.run("textField");
-    }
-}
---
+++ src/sae_g3s2e2/SAE_G3S2E2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sae_g3s2e2;
+
+
+/**
+ *
+ * @author donat
+ */
+public class SAE_G3S2E2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {      
+    }
+    
+}
