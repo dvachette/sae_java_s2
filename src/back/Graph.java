d30d0497ae092bf7a3d5b07416b81167ba36c070
Donatien Vachette <donatien.vachette@hotmail.com>
2025-05-25 01:47:53 +0200

    [MODIFICATIONS] Graph - Renomage - Type générique poir les points - Génération de points aléatoires

---
+++ src/back/Graph.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package back;
+
+import java.util.Collections;
+import java.util.Random;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author donat
+ * @param <T>
+ */
+public class Graph<T extends Point> {
+
+    private TreeMap<Integer, T> points;
+
+    public Graph() {
+        this.points = new TreeMap<>();
+    }
+
+    public TreeMap<Integer, T> getPoints() {
+        return points;
+    }
+
+    public void setPoints(TreeMap<Integer, T> points) {
+        this.points = points;
+    }
+
+    public T getPoint(int id) {
+        return points.get(id);
+    }
+
+    public T getPoint(double x_or_latitude, double y_or_longitude) {
+        // I know this code is horible but i can't figure a better way...
+        T ans = null;
+        for (T point : points.values()) {
+            if (point instanceof PointEuclidien) {
+                PointEuclidien pe = (PointEuclidien) point;
+                if ((pe.getX() == x_or_latitude) && (pe.getY() == y_or_longitude)) {
+                    ans = point;
+                }
+            } else if (point instanceof PointGeographique) {
+                PointGeographique pg = (PointGeographique) point;
+                if ((pg.getLatitude() == x_or_latitude) && (pg.getLongitude() == y_or_longitude)) {
+                    ans = point;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public void addPoint(T p) {
+        points.put(p.getId(), p);
+    }
+
+    public TreeMap<Integer, Double> getDistances(T p) {
+        TreeMap<Integer, Double> map = new TreeMap<>();
+        for (int i : points.keySet()) {
+            map.put(i, p.distanceOf(points.get(i)));
+        }
+        return map;
+    }
+
+    public TreeMap<Integer, Double> getDistances(int id) {
+        return this.getDistances(this.getPoint(id));
+    }
+
+    public TreeMap<Integer, TreeMap<Integer, Double>> getDistancesTable() {
+        TreeMap<Integer, TreeMap<Integer, Double>> map = new TreeMap<>();
+        for (int i : points.keySet()) {
+            map.put(i, getDistances(i));
+        }
+        return map;
+    }
+    
+    public static Graph<PointEuclidien> randomPointSet(int count, double minX, double maxX, double minY, double maxY) {
+        Graph<PointEuclidien> graph = new Graph<>();
+        Random rng = new Random();
+        for (int c = 1; c <= count; c++) { // Here is C++ in JAVA ;)
+            graph.addPoint(new PointEuclidien(rng.nextDouble(minX, maxX), rng.nextDouble(minY, maxY), c));
+        }
+        return graph;
+    }
+    
+    public static Graph<PointEuclidien> randomPointSet(int count) {
+        return Graph.randomPointSet(count, 0, 100, 0, 100);
+    }
+    
+    public int maxIdValue() {
+        return Collections.max(points.keySet());
+    }
+}
