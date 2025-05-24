7997c99175d3ec738de5eb8d9f93e949a63c1983
Donatien Vachette <donatien.vachette@hotmail.com>
2025-05-25 01:47:08 +0200

    [MODIFICATIONS] Main Tests en cours

--- src/sae_g3s2e2/SAE_G3S2E2.java
+++ src/sae_g3s2e2/SAE_G3S2E2.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package sae_g3s2e2;
+
 import back.*;
 
 /**
@@ -14,21 +15,14 @@ public class SAE_G3S2E2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {     
-        PointEuclidien p1 = new PointEuclidien(0, 0, 1);
-        PointEuclidien p2 = new PointEuclidien(4, 3, 2);
-        PointEuclidien p3 = new PointEuclidien(1, 1, 3);
-        PointEuclidien p4 = new PointEuclidien(0, -3, 4);
-        PointEuclidien p5 = new PointEuclidien(-40, 30, 5);
+    public static void main(String[] args) {
+        Graph<PointEuclidien> graph = Graph.randomPointSet(10);
+        System.out.println(graph.getPoint(2));
+        System.out.println(graph.getPoint(4));
+        System.out.println(graph.getDistances(2));
         
-        GraphEuclidien graph = new GraphEuclidien();
-        graph.addPoint(p1);
-        graph.addPoint(p2);
-        graph.addPoint(p3);
-        graph.addPoint(p4);
-        graph.addPoint(p5);
-        
-        System.out.println(graph.getDistancesTable());
+
+
     }
-    
+
 }
