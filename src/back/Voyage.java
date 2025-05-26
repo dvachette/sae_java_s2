2c6fff3a67cca22d374966c4b124a19e18bf326b
ChatonMignon134 <cedrethan@orange.fr>
2025-05-27 01:46:51 +0200

    Il est 2h du mat j'écoute du lalaland avec mon stitch de soutien émotionnel MAIS le code marche

---
+++ src/back/Voyage.java
@@ -0,0 +1,27 @@
+package back;
+
+
+/**
+ * @author Ethan
+ */
+
+public class Voyage {
+    String name;
+    String type;
+    String comment;
+    int dimension;
+    String Type_Coordinate;
+    String Display_Type;
+    String EDGE_WEIGHT_FORMAT;
+
+
+    public Voyage(){
+        this.name = "";
+        this.type = "";
+        this.comment = "";
+        this.dimension = 0;
+        this.Type_Coordinate = "";
+        this.Display_Type = "";
+        this.EDGE_WEIGHT_FORMAT = "";
+    }
+}
